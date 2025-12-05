2a0a5b87dcdfcf13cd09934fe1bf4ea84d4ed84b
Filipe <filipe.figueiroa@outlook.com>
2025-12-05 07:24:25 -0300

    Novos Exercicios

---
+++ task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task12.task1207;
+
+/* 
+int and Integer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(5);
+        print((Integer) 5);
+    }
+
+    //write your code here
+    public static void print(int a) {
+    }
+
+    public static void print(Integer a) {
+    }
+}
