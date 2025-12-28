4f7f918b2584121c7ce2d29a375f258c8d3ddf77
Filipe <filipe.figr@gmail.com>
2025-12-27 23:04:07 -0300

    Novos Exercicios

---
+++ task/task17/task1701/Solution.java
@@ -0,0 +1,60 @@
+package com.codegym.task.task17.task1701;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Notes
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new NoteThread().start();
+        new NoteThread().start();
+    }
+
+    public static class NoteThread extends Thread{
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                String tempNote = this.getName() + "-Note" + i;
+                Note.addNote(tempNote);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                try {
+                    Note.removeNote(this.getName());
+                } catch (IndexOutOfBoundsException e){
+                    continue;
+                }
+            }
+
+        }
+    }
+
+    public static class Note {
+
+        public static final List<String> notes = new ArrayList<>();
+
+        public static void addNote(String note) {
+            notes.add(0, note);
+        }
+
+        public static void removeNote(String threadName) {
+            String note = notes.remove(0);
+            if (note == null) {
+                System.out.println("Another thread deleted our note");
+            } else if (!note.startsWith(threadName)) {
+                System.out.println("Thread [" + threadName + "] deleted someone else's note [" + note + "]");
+            } else {
+                System.out.println("Thread [" + threadName + "] deleted its own note [" + note + "]");
+            }
+        }
+    }
+}
